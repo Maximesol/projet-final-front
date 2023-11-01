b344980ad6e3314fcc7d4c758cbf26521f660fcd
Maximesol <maxime.gognies@gmail.com>
2023-11-01 11:25:10 +0100

    📦 🚧 F01: Create <HomePage> + add styled component package

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function HomePage() {
+    return (
+        <HomePageStyled>HomePage</HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.div`
+  
+`;
