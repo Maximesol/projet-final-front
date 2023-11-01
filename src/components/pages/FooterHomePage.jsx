c35385a7f43e9d2cb4e5bd440d14aeed8b929432
Maximesol <maxime.gognies@gmail.com>
2023-11-01 15:10:16 +0100

    💄 F01: Add FooterHomePage, HeaderHomePage

---
+++ src/components/pages/FooterHomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function FooterHomePage() {
+    return (
+        <FooterHomePageStyled>FooterHomePage</FooterHomePageStyled>
+    )
+}
+
+const FooterHomePageStyled = styled.div`
+background-color: #1b2030;
+flex: 0.35;
+`;
